525c65bb4f2227c78fc8b6a46fab4b40c839317c
Roelant Vos <roelant@ravos.nl>
2019-07-05 16:32:22 +1000

    Update ClassTextHandling.cs

--- Virtual_EDW/Classes/ClassTextHandling.cs
+++ Virtual_EDW/Classes/ClassTextHandling.cs
@@ -11,6 +11,53 @@ namespace Virtual_EDW
 {
     class TextHandling
     {
+        /// <summary>
+        ///   Retrieves the handlebars variables from a given text box / text.
+        /// </summary>
+        /// <param name="inputTextBox"></param>
+        /// <param name="inputText"></param>
+        /// <returns></returns>
+        internal static List<string> HandleBarsVariableList(RichTextBox inputTextBox, string inputText)
+        {
+            List<String> variableList = new List<string>();
+
+            inputTextBox.Clear();
+
+            // Split the text into lines, so we can parse each line
+            Regex splitLineRegex = new Regex("\\n");
+            String[] lines = splitLineRegex.Split(inputText);
+
+            // Outer loop - running through the lines
+            foreach (string line in lines)
+            {
+                // Breaking the line up in elements (tokens)
+                Regex wordRegex = new Regex("([ \\t{}():;])");
+                String[] tokens = wordRegex.Split(line);
+
+                int counter = 0;
+
+                foreach (string token in tokens)
+                {
+                    if (token == "{")
+                    {
+                        counter++;
+                    }
+
+                    if (counter > 1 && token != "{" && token != "}")
+                    {
+                        variableList.Add("{{"+token+"}}");
+                    }
+
+                    if (token == "}")
+                    {
+                        counter = 0; //Reset the counter
+                    }
+                }
+            }
+
+            return variableList;
+        }
+
         internal static void SyntaxHighlightHandlebars(RichTextBox inputTextBox, string inputText)
         {
             inputTextBox.Clear();
