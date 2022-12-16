fe70c3961e0d393e32fe20fb41baef7d39a44b22
Roelant Vos <roelant@ravos.nl>
2022-12-16 11:38:21 +1000

    Update LoadPatternGrid.cs

--- Virtual_EDW/LoadPatternGrid.cs
+++ Virtual_EDW/LoadPatternGrid.cs
@@ -17,7 +17,7 @@ namespace Virtual_Data_Warehouse
             EditMode = DataGridViewEditMode.EditOnEnter;
 
             Location = new Point(3, 191);
-            Size = new Size(1215, 521);
+            Size = new Size(1215, 325);
             BackgroundColor = SystemColors.AppWorkspace;
             GridColor = SystemColors.ControlDark;
 
